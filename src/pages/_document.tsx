b23e3e0f9c9298075e49487ef917a79ce4fcf0f8
lhjwork <jin.come.up.business@gmail.com>
2025-10-28 10:56:56 +0900

    add.txt and script add

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,11 @@ class MyDocument extends Document {
             name="google-adsense-account"
             content="ca-pub-1861714561784170"
           ></meta>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1861714561784170"
+            crossOrigin="anonymous"
+          ></script>
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
